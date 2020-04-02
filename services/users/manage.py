c4c944c8a57ff014cfd02ec719ec6cfaf15c9f10
Harry Wang <harryjwang@gmail.com>
2020-04-02 13:13:43 -0400

    updated flask admin, flask-restx to support werkzeug 1.0.0

--- services/users/manage.py
+++ services/users/manage.py
@@ -21,8 +21,8 @@ def recreate_db():
 
 @cli.command('seed_db')
 def seed_db():
-    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
-    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.add(User(username='michael', email="hermanmu@gmail.com", password="mypassword"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org", password="mypassword"))
     db.session.commit()
 
 
