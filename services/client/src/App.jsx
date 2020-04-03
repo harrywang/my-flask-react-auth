e28bd8e8a2b0e2818ae78ac2f59c8b68a36dc4ed
Harry Wang <harryjwang@gmail.com>
2020-04-03 13:44:18 -0400

    added react bulma

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -5,6 +5,9 @@ import {Route, Switch} from 'react-router-dom';
 import UsersList from "./components/UsersList";
 import AddUser from "./components/AddUser";
 import About from './components/About';
+import NavBar from './components/NavBar';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
 
 class App extends Component {
   constructor() {
@@ -13,7 +16,8 @@ class App extends Component {
     this.state = {
       users: [],
       username: "",
-      email: ""
+      email: "",
+      title: "My App",
     };
 
     this.addUser = this.addUser.bind(this);
@@ -56,37 +60,42 @@ class App extends Component {
 
   render() {
     return (
-      <section className="section">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-half">
-              <br />
-              <Switch>
-                <Route
-                  exact="exact"
-                  path='/'
-                  render={() => (
-                    <div>
-                      <h1 className="title is-1">Users</h1>
-                      <hr /><br />
-                      <AddUser
-                        username={this.state.username}
-                        email={this.state.email}
-                        addUser={this.addUser}
-                        // eslint-disable-next-line react/jsx-handler-names
-                        handleChange={this.handleChange}
-                      />
-                      <br /><br />
-                      <UsersList users={this.state.users} />
-                    </div>
-                  )}
-                />
-                <Route exact="exact" path='/about' component={About} />
-              </Switch>
+      <div>
+        <NavBar title={this.state.title} />
+        <section className="section">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-half">
+                <br />
+                <Switch>
+                  <Route
+                    exact
+                    path="/"
+                    render={() => (
+                      <div>
+                        <h1 className="title is-1">Users</h1>
+                        <hr /><br />
+                        <AddUser
+                          username={this.state.username}
+                          email={this.state.email}
+                          addUser={this.addUser}
+                          // eslint-disable-next-line react/jsx-handler-names
+                          handleChange={this.handleChange}
+                        />
+                        <br /><br />
+                        <UsersList users={this.state.users} />
+                      </div>
+                    )}
+                  />
+                  <Route exact path='/register' component={RegisterForm} />
+                  <Route exact path='/login' component={LoginForm} />
+                  <Route exact path='/about' component={About} />
+                </Switch>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     );
   }
 }
